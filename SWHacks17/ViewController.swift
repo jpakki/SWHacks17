245db0eb510bea5128f76a5de43cf77facbfe50a
Jaswant Pakki <jpakki1@asu.edu>
2017-03-10 20:07:47 -0700

    intial commit

---
+++ SWHacks17/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWHacks17
+//
+//  Created by Jaswant Pakki on 3/10/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
